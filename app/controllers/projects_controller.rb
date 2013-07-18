0844ef9154ab90421e360c7fa8b4f8d90fbefd55
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-18 14:40:17 -0700

    Make projects only visible to users with permisstion to see them

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,7 @@
 class ProjectsController < ApplicationController
   before_filter :authorize_admin!, :except => [:index, :show]
-	before_filter :find_project, :only => [:show,
-                                       :edit,
-                                       :update,
-                                       :destroy]
+  before_filter :authenticate_user!, :only => [:show]
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
 
 	def index
 		@projects = Project.all
@@ -30,28 +28,32 @@ class ProjectsController < ApplicationController
 	def edit
 	end
 
-def update
-  if @project.update_attributes(params[:project])
-    flash[:notice] = "Project has been updated."
-    redirect_to @project
-  else
-    flash[:alert] = "Project has not been updated."
-    render :action => "edit"
+  def update
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render :action => "edit"
+    end
   end
-end
 
-def destroy
-	@project.destroy
-	flash[:notice] = "Project has been deleted."
-	redirect_to projects_path
-end
+  def destroy
+	 @project.destroy
+	 flash[:notice] = "Project has been deleted."
+	 redirect_to projects_path
+  end
 
 private
 	def find_project
-  	@project = Project.find(params[:id])
+  	@project = if current_user.admin?
+      Project.find(params[:id])
+    else
+      Project.viewable_by(current_user).find(params[:id])
+    end
   	rescue ActiveRecord::RecordNotFound
-  	flash[:alert] = "The project you were looking" +
+  	 flash[:alert] = "The project you were looking" +
                     " for could not be found."
-  	redirect_to projects_path
+  	 redirect_to projects_path
 	end
 end
